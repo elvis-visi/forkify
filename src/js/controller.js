b3dc0f212990d74f180f3392b6a09eb3b01cfebf
elvis-visi <sivisevi1@gmail.com>
2023-10-14 16:07:49 +0200

    3. Loading a Recipe from API

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,39 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+console.log(`Test`);
+
+const showRecipe = async function () {
+  try {
+    //fetch request to the api, response object back
+    const res = await fetch(
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcb37'
+    );
+    //convert to JSON
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+
+    //Format the data -> create new object
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+
+    console.log(res, data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+showRecipe();
