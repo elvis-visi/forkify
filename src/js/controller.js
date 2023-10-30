585ce5484e1a88d77421ebde721a731ee366363b
elvis-visi <sivisevi1@gmail.com>
2023-10-30 13:41:01 +0100

    13. Implementing Bookmarks, PreviewView

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView';
 import searchView from './views/searchView';
 import resultsView from './views/resultsView';
 import paginationView from './views/paginationView';
+import bookmarksView from './views/bookmarksView';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -26,6 +27,7 @@ const controlRecipes = async function () {
 
     // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     //1) Loading recipe
     await model.loadRecipe(id);
@@ -82,11 +84,15 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  //1) Add/remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
 
-  console.log(model.state.recipe);
+  //2) Update recipe view
   recipeView.update(model.state.recipe);
+
+  //3) Render bookmakrs
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
